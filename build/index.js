6754735d19c865cc9a8def14541f7484a36aa212
Akash Patel <pppatelakash@gmail.com>
2023-02-04 10:44:55 +0530

    Create POST request the endpoint

--- build/index.js
+++ build/index.js
@@ -4,10 +4,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var uuid_1 = require("uuid");
+var Gender;
+(function (Gender) {
+    Gender["Male"] = "Male";
+    Gender["Female"] = "Female";
+    Gender["Other"] = "Other";
+})(Gender || (Gender = {}));
 var app = (0, express_1.default)();
+var patients = [];
+app.use(express_1.default.json());
 var port = process.env.PORT || 3000;
-app.get('/api/ping', function (req, res) {
-    res.send('pong');
+app.get("/api/ping", function (req, res) {
+    res.send("pong");
+});
+app.post("/api/patients", function (req, res) {
+    var patient = req.body;
+    var name = patient.name, gender = patient.gender;
+    if (!name || !gender) {
+        return res
+            .status(400)
+            .send({ error: "Patient name and gender are required." });
+    }
+    if (gender !== Gender.Male &&
+        gender !== Gender.Female &&
+        gender !== Gender.Other) {
+        return res.status(400).send({ error: "Invalid gender value." });
+    }
+    var newPatient = { id: (0, uuid_1.v1)(), name: name, gender: gender };
+    patients.push(newPatient);
+    return res.status(201).send(newPatient);
 });
 app.listen(port, function () {
     console.log("Server listening at http://localhost:".concat(port));
--- build/index.ts
+++ build/index.ts
@@ -1,10 +1,51 @@
-import express from 'express';
+import express from "express";
+import { v1 as uuid } from "uuid";
+
+enum Gender {
+  Male = "Male",
+  Female = "Female",
+  Other = "Other",
+}
+
+interface Patient {
+  id: string;
+  name: string;
+  gender: Gender;
+}
 
 const app = express();
+const patients: Patient[] = [];
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
-app.get('/api/ping', (req, res) => {
-  res.send('pong');
+app.get("/api/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.post("/api/patients", (req, res) => {
+  
+  
+  const patient: Patient = req.body;
+  const { name, gender } = patient;
+
+  if (!name || !gender) {
+    return res
+      .status(400)
+      .send({ error: "Patient name and gender are required." });
+  }
+
+  if (
+    gender !== Gender.Male &&
+    gender !== Gender.Female &&
+    gender !== Gender.Other
+  ) {
+    return res.status(400).send({ error: "Invalid gender value." });
+  }
+
+  const newPatient = { id: uuid(), name, gender };
+  patients.push(newPatient);
+
+  return res.status(201).send(newPatient);
 });
 
 app.listen(port, () => {
